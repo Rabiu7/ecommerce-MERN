860b445947f8bc69ff32b049b37471fde854cf32
hansa yasar <Rabiu7>
2026-09-20 00:15:47 +0530

    CORS Update

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 // =========================================================
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://ecommerce-mern-hazel.vercel.app"],
+  origin: ["http://localhost:5173", "https://www.mashaallahcreations.in"],
   credentials: true,
 };
 
