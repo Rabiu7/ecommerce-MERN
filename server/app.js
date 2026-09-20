186d01659ceb8e5ff4e2b3fbfc297baefca1753d
hansa yasar <Rabiu7>
2026-09-20 14:12:08 +0530

    Disable Log

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+if (process.env.NODE_ENV === "production") {
+  console.log = () => {};
+}
+
 dotenv.config();
 
 const app = express();
